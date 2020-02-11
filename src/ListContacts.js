e8959a58e21b4a971c32e8c2f5cffda39f6a3a51
Haci Karahasanoglu <haci@hacikho.com>
2020-02-10 23:30:35 -0500

    Pass datainto components with props

---
+++ src/ListContacts.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class ListComponents extends Component{
+    render(){
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map((contacts)=>(
+                    <li key={contacts.id} className='contact-list-item'>
+                        <div className='contact-avatar'
+                            style={{backgroundImage:`url(${contacts.avatarURL})`}}
+                            ></div>
+                        <div className='contact-details'>
+                            <p>{contacts.name}</p>
+                            <p>@{contacts.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+        )
+    }
+}
+
+export default ListComponents
