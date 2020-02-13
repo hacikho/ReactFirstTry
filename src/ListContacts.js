cea03b90f1444391d17b6666592e4ec1a03c6a27
Haci Karahasanoglu <55646@fenetwork.com>
2020-02-13 11:23:49 -0500

    Update state with setState

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function ListContacts(props){
-        return (
-            <ol className='contact-list'>
-                {props.contacts.map((contacts)=>(
-                    <li key={contacts.id} className='contact-list-item'>
-                        <div className='contact-avatar'
-                            style={{backgroundImage:`url(${contacts.avatarURL})`}}
-                            ></div>
-                        <div className='contact-details'>
-                            <p>{contacts.name}</p>
-                            <p>@{contacts.handle}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((contacts) => (
+                <li key={contacts.id} className='contact-list-item'>
+                    <div className='contact-avatar'
+                        style={{ backgroundImage: `url(${contacts.avatarURL})` }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contacts.name}</p>
+                        <p>@{contacts.handle}</p>
+                    </div>
+                    <button
+                        onClick={() => props.onDeleteContact(contacts)}
+                        className='contact-remove'>
+                        Remove
                         </button>
-                    </li>
-                ))}
-            </ol>
-        )
+                </li>
+            ))}
+        </ol>
+    )
 }
 export default ListContacts
